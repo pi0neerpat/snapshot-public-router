38bae311edd7d52b41093cf949976300d9851703
Patrick <blockchainbuddha@gmail.com>
2020-10-01 11:08:08 -0400

    update landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet';
-import '../styles/index.css';
+import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import "../styles/index.css";
 
 function Index() {
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/date');
+      const res = await fetch("/api/date");
       const newDate = await res.text();
       setDate(newDate);
     }
@@ -15,40 +15,27 @@ function Index() {
   return (
     <main>
       <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
+        <title>Snapshot Public Router</title>
       </Helmet>
-      <h1>Gatsby + Node.js (TypeScript) API</h1>
+      <h1>Snapshot Public Router</h1>
       <h2>
-        Deployed with{' '}
+        Forwards all events from{" "}
         <a
-          href="https://vercel.com/docs"
+          href="https://snapshot.page"
           target="_blank"
           rel="noreferrer noopener"
         >
-          Vercel
-        </a>
-        !
+          ⚡ Snapshot
+        </a>{" "}
+        to your app.
       </h2>
       <p>
-        <a
-          href="https://github.com/vercel/vercel/blob/master/gatsby"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
+        Want to receive Snapshot events? Please make a Pull Request here:
+        <br />
+        <a href="https://github.com/pi0neerpat/snapshot-public-router">
+          https://github.com/pi0neerpat/snapshot-public-router
         </a>
-        .
       </p>
-      <br />
-      <h2>The date according to Node.js (TypeScript) is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
 }
