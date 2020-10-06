668df35f8c259f0041f2c8b325efec10078251c0
alok tiwari <aloksstiwari@gmail.com>
2020-10-06 22:56:19 +0530

    Added collabland node-red webhook app url

--- approvedAppList.js
+++ approvedAppList.js
@@ -3,5 +3,9 @@ const approvedAppList = [
     name: "Collab Land",
     endpoint: "https://bot-listener.collab.land",
   },
+  {
+    name: "CollabLand NodeRed",
+    endpoint: "https://prodred.collab.land/snapshot",
+  },
 ];
 export default approvedAppList;
