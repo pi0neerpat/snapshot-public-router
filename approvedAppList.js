1e15c8c579362225a1627225da2c9bf3cac9eb2c
alok tiwari <aloksstiwari@gmail.com>
2020-10-06 23:00:10 +0530

    Added app list for snapshot events

--- approvedAppList.js
+++ approvedAppList.js
@@ -1,9 +1,5 @@
 const approvedAppList = [
   {
-    name: "Collab Land",
-    endpoint: "https://bot-listener.collab.land",
-  },
-  {
     name: "CollabLand NodeRed",
     endpoint: "https://prodred.collab.land/snapshot",
   },
