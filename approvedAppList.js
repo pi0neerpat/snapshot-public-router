a655982cac92630a20370b05534edf30bb6ceebe
Patrick <blockchainbuddha@gmail.com>
2020-10-12 09:54:08 -0400

    add test endpoint

--- approvedAppList.js
+++ approvedAppList.js
@@ -3,5 +3,10 @@ const approvedAppList = [
     name: "CollabLand NodeRed",
     endpoint: "https://prodred.collab.land/snapshot",
   },
+  {
+    name:
+      "(test site) https://webhook.site/de3d4d28-4da0-474c-ac88-8053dca23934",
+    endpoint: "https://webhook.site/de3d4d28-4da0-474c-ac88-8053dca23934",
+  },
 ];
 export default approvedAppList;
